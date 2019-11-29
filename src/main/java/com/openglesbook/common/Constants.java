7f2ace9fac50829c4cf783b6e77ca7708fae4e88
LiujinsongRR <liujinsong@rockrobo.com>
2019-11-29 17:38:21 +0800

    增加常量

--- src/main/java/com/openglesbook/common/Constants.java
+++ src/main/java/com/openglesbook/common/Constants.java
@@ -2,4 +2,6 @@ package com.openglesbook.common;
 
 public class Constants {
     public static final int BYTES_PER_FLOAT = 4;
+    //每个位置3个浮点数
+    public static final int POS_COMPONENT_SIZE = 3;
 }
